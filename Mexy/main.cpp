34e736c1fe353b08b7025e9d64b0e93573d750c2
saranghan <sarah.sarang.han@gmail.com>
2019-08-09 09:47:34 +1200

    Initial Commit

---
+++ Mexy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mexy
+//
+//  Created by Sarang Han on 8/9/19.
+//  Copyright © 2019 Sarang Han. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
